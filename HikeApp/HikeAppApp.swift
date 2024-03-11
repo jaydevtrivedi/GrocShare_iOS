967bc9484ccfccf980a12bb02b97a89292985e95
Jaydev Trivedi <45784839+jaydevtrivedi@users.noreply.github.com>
2024-03-11 11:05:04 +1100

    Initial Commit

---
+++ HikeApp/HikeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeAppApp.swift
+//  HikeApp
+//
+//  Created by Jaydev Trivedi on 11/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct HikeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
